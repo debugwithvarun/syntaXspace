39e77f4baa89119aa09c72fb8dc24b6ff635c970
Varunpython028 <varun_mit@techhubtechnology.com>
2025-11-04 14:02:04 +0530

    Add new Files

--- frontend/src/Pages/AppDashboard.tsx
+++ frontend/src/Pages/AppDashboard.tsx
@@ -3,8 +3,7 @@ import LeftSideSection from "@/components/dashboard/leftsection/LeftSideSection"
 import VerifyEmail from "@/components/navbar/VerifyEmail"
 import RightSideSection from "@/components/dashboard/rightsection/RightSideSection"
 import NewNavbar from "@/components/navbar/NewNavbar"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Card } from "@/components/ui/card"
+import MiddleSection from "@/components/dashboard/middlesection/MiddleSection"
 
 
 
@@ -15,11 +14,7 @@ const AppDashboard = () => (
     <VerifyEmail />
     <div className="w-full h-full bg-(--color-muted) flex max-sm:flex-col px-4 md:px-6 gap-4 justify-between">
       <LeftSideSection />
-      <ScrollArea className="h-full w-full p-6  pt-10 ">
-            <Card className="">
-
-            </Card>
-      </ScrollArea>
+       <MiddleSection/> 
       <RightSideSection />
 
     </div>
---
+++ frontend/src/components/dashboard/middlesection/MiddleSection.tsx
@@ -0,0 +1,15 @@
+import { Card } from '@/components/ui/card'
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+
+const MiddleSection = () => {
+  return (
+    <ScrollArea className="h-full w-full p-6  pt-10 ">
+    <Card className="">
+
+    </Card>
+</ScrollArea>
+  )
+}
+
+export default MiddleSection
